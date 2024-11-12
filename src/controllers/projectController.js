edc521edbe05b908eb8eaa5be330ec29adf9ef82
felipedestroo <fdestrocontact@gmail.com>
2024-11-12 19:47:24 -0300

    feat: first steps

---
+++ src/controllers/projectController.js
@@ -0,0 +1,12 @@
+import projectService from '../services/projectService.js';
+
+export const createProject = async (req, res) => {
+  const { name } = req.body;
+  const project = await projectService.createProject(name);
+  res.status(201).json(project);
+};
+
+export const getAllProjects = async (req, res) => {
+  const projects = await projectService.getAllProjects();
+  res.status(200).json(projects);
+};
---
+++ src/services/authService.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import User from '../models/User.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const authenticateUser = async (username, password) => {
+  const user = await User.findOne({ where: { username } });
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+    return token;
+  }
+  throw new Error('Authentication failed');
+};
